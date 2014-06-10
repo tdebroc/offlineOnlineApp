fd00e0684f1a00e76ca0c1c829cae86d9c2312ea
Thibaut de Broca <tdebroc@gmail.com>
2014-06-10 20:05:47 +0200

    Revert "Revert "No global variable for dbManager""
    This reverts commit 9e7f4fe680af9f23dd411defc167745358733b46.

--- war/static/js/main.js
+++ war/static/js/main.js
@@ -6,7 +6,7 @@ xhr = new XMLHttpRequest();
  * Called when DOM has been loaded.
  */
 function loaded() {
-  window.dataBaseManager = new DataBaseManager(MODEL_NAME);
+  var dataBaseManager = new DataBaseManager(MODEL_NAME);
   dataBaseManager.loadDatas();
   $('#launchFullSynch').click(function() {
     dataBaseManager.reloadDatas();
